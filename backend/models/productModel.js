c13632f5033ccc057ccad6cba07f105a7006827d
PKBAJPAI12 <prateek.cse.uiet@gmail.com>
2023-09-25 00:18:32 +0530

    mongoose product model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,51 +1,67 @@
-const mongoose=require('mongoose');
-const productSchema=mongoose.Schema({
-    name:{
-
+const mongoose = require("mongoose");
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter Product Name"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please Enter Product Description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please Enter Product Price"],
+    maxLength: [8, "Price cannot exceed 8 characters"],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
     },
-    description:{
-
+  ],
+  category: {
+    type: String,
+    required: [true, "Please Enter Product Category"],
+  },
+  Stock: {
+    type: Number,
+    required: [true, "Please Enter Product Stock"],
+    maxLength: [4, "Stock can't exceed 4 Character"],
+    default: 1,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
     },
-    price:{
-      required:[true,"Please Enter Product Price"],  
-      maxLength:[8,"Price cannot exceed 8 characters"]
-    },
-    rating:{
-      default:0
-    },
-    images:[
-        {
-            public_id:{
-                type:String,
-                required:true
-            },
-            url:{
-                type:String,
-                required:true
-            }
-        }
-    ],
-    category:{
-        type:String,
-        required:[true,"Please Enter Product Category"]
-    },
-    Stock:{
-        type:Number,
-        required:[true,""],
-        maxLength:[4,""],
-        default:1
-    },
-    reviews:[
-        {
-            name:{
-
-            },
-            rating:{
-
-            },
-            comment:{
-                
-            }
-        }
-    ]
-})
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model("Product", productSchema);
