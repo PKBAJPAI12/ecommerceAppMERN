6aa5e30619520c4872f7bea825a60e831480d9a9
PKBAJPAI12 <prateek.cse.uiet@gmail.com>
2023-10-26 16:45:53 +0530

    create api routes for create review,delete, get reviews

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -51,7 +51,6 @@ exports.updateProduct = catchAsyncError(async (req, res, next) => {
   });
 });
 //delete product
-
 exports.deleteProduct=catchAsyncError(async(req,res,next)=>{
     console.log(req.params.id);
     const product=await Product.findById(req.params.id);
@@ -69,3 +68,81 @@ exports.deleteProduct=catchAsyncError(async(req,res,next)=>{
         message:"Product Deleted Successfully"
       });
 });
+exports.createProductReview = catchAsyncError(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  const isReviewed = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  if (isReviewed) {
+    product.reviews.forEach((rev) => {
+      if (rev.user.toString() === req.user._id.toString())
+        (rev.rating = rating), (rev.comment = comment);
+    });
+  } else {
+    product.reviews.push(review);
+    product.numOfReviews = product.reviews.length;
+  }
+
+  let avg = 0;
+
+  product.reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
+
+  product.ratings = avg / product.reviews.length;
+
+  await product.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// Get All Reviews of a product
+exports.getProductReviews = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+});
+exports.deleteReview=catchAsyncError(async(req,res,next)=>{
+  const product=await Product.findById(req.query.productId);
+  if(!product){
+    return next(new ErrorHandler("product not found",404));
+  }
+  const reviews=product.reviews.filter((rev)=>rev._id.toString!=req.query.id.toString);
+  let avg=0;
+  reviews.forEach((rev)=>{avg+=rev.rating;});
+  const ratings=avg/reviews.length;
+  const numOfReviews=reviews.length;
+  await product.findByIdAndUpdate(req.query.productId,{
+    reviews,
+    ratings,
+    numOfReviews,
+  },
+  {
+    new:true,
+    runValidators:true,
+    useFindAndModify:false,
+  });
+res.status(200).json({
+  success:true,
+})
+})
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,13 @@
 const express=require('express');
 const router=express.Router();
-const  {getAllProducts,getProductDetails,createProduct,updateProduct,deleteProduct}  = require('../controller/productController');
+const  {getAllProducts,getProductDetails,createProduct,updateProduct,deleteProduct,createProductReview, getProductReviews,deleteReview}  = require('../controller/productController');
 const { isAuthenticatedUser,authorisedRole } = require('../middleware/auth');
 router.get('/products',getAllProducts);
 router.post('/product/new',isAuthenticatedUser,authorisedRole("admin"),createProduct);
 router.put('/product/:id',isAuthenticatedUser,authorisedRole("admin"),updateProduct);
 router.delete('/product/:id',isAuthenticatedUser,authorisedRole("admin"),deleteProduct);
 router.get('/product/:id',getProductDetails);
+router.put('/reviews',isAuthenticatedUser,createProductReview);
+router.get('/reviews',getProductReviews);
+router.delete('/reviews',isAuthenticatedUser,deleteReview);
 module.exports=router
