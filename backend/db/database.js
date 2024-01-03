9352f6fcc485d531958e4b88cd1830d7adf20252
PKBAJPAI12 <prateek.cse.uiet@gmail.com>
2024-01-04 01:15:40 +0530

    change in dbconnect

--- backend/db/database.js
+++ backend/db/database.js
@@ -13,6 +13,6 @@ const connectDB=()=>{
     mongoose.connect(process.env.DB_URL, options)
     .then((data) => {
       console.log(`Connected to MongoDB ${data.connection.host}`);
-    }); 
+    }).catch(error => console.error('Error connecting to MongoDB:', error));; 
 }
 module.exports=connectDB
