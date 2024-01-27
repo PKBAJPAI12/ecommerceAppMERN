ad52d1b47c5b21e3e2afc2f84fc5402adaf378c1
PKBAJPAI12 <prateek.cse.uiet@gmail.com>
2024-01-27 15:50:37 +0530

    cors added

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,12 @@ const express=require('express');
 const app=express();
 const cookieParser=require('cookie-parser');
 const bodyParser=require('body-parser');
+const cors = require('cors');
 const fileUpload=require('express-fileupload')
 const errorMiddleware=require('./middleware/error');
 const dotenv=require('dotenv');
 dotenv.config({path:"backend/.env"});
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
