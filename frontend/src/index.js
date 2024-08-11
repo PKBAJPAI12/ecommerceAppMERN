d9da34ad1680ec5596abc1e6833ee34d0c718054
PKBAJPAI12 <prateek.cse.uiet@gmail.com>
2024-08-12 01:56:20 +0530

    index file update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import './App.css';
 import {Provider} from "react-redux";
 import store from './store';
 import { positions, transitions, Provider as AlertProvider } from "react-alert";
