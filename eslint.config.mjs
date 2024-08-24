6604723e465c93955d3b1850fd537afebb3a9d3a
PKBAJPAI12 <prateek.cse.uiet@gmail.com>
2024-08-24 01:54:39 +0100

    package json update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,10 @@
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 
+
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: "module",
-      globals: globals.browser
-    },
-    plugins: {
-      react: pluginReact
-    },
-    rules: {
-      "react/prop-types": "off"
-    },
-    settings: {
-      react: {
-        version: "detect"
-      }
-    }
-  },
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      sourceType: "commonjs"
-    },
-    rules: {
-      
-    }
-  }
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginReact.configs.flat.recommended,
 ];
